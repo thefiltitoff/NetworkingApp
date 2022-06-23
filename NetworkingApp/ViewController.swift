2178d8cb08b0a4dc351b7a7b3d2ea4016d3efe8d
Felix Titov <titow.feliks@yandex.ru>
2022-06-23 22:51:30 +0300

    Created singleton urlSession to download photo

--- NetworkingApp/ViewController.swift
+++ NetworkingApp/ViewController.swift
@@ -14,10 +14,35 @@ class ViewController: UIViewController {
     @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var label: UILabel!
     @IBOutlet weak var getImageButton: UIButton!
-
-    @IBAction func getImagePressed(_ sender: Any) {
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        activityIndicator.isHidden = true
+        activityIndicator.hidesWhenStopped = true
     }
     
+    @IBAction func getImagePressed(_ sender: Any) {
+        
+        label.isHidden = true
+        getImageButton.isEnabled = false
+        activityIndicator.isHidden = false
+        activityIndicator.startAnimating()
+        
+        guard let url = URL(string: "https://applelives.com/wp-content/uploads/2016/03/iPhone-SE-11.jpeg") else { return }
+        
+        let session = URLSession.shared
+        session.dataTask(with: url) { data, response, error in
+            if let data = data, let image = UIImage(data: data) {
+                DispatchQueue.main.async {
+                    self.imageView.image = image
+                    self.activityIndicator.stopAnimating()
+                }
+            }
+        }.resume()
+        
+        
+    }
 }
 
 
