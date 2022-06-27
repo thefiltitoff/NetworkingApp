037fab6179b9c149ac312b8aff3f398891e86eb1
Felix Titov <titow.feliks@yandex.ru>
2022-06-27 23:16:09 +0300

    Created and tested GET request

--- NetworkingApp/ViewController.swift
+++ NetworkingApp/ViewController.swift
@@ -12,7 +12,21 @@ import UIKit
 class ViewController: UIViewController {
     
     @IBAction func getRequest(_ sender: Any) {
+        guard let url = URL(string: "https://jsonplaceholder.typicode.com/posts")
+        else { return }
         
+        let session = URLSession.shared
+        session.dataTask(with: url) { data, response, error in
+            guard let response = response,
+                let data = data else { return }
+            
+            do {
+                let json = try JSONSerialization.jsonObject(with: data, options: [])
+                print(json)
+            } catch {
+              print(error)
+            }
+        }.resume()
     }
     
     @IBAction func postRequest(_ sender: Any) {
