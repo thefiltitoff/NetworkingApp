0fa29265c260f1d17993554aee07cc9cd80483bf
Felix Titov <titow.feliks@yandex.ru>
2022-06-27 23:51:49 +0300

    Created POST Request

--- NetworkingApp/ViewController.swift
+++ NetworkingApp/ViewController.swift
@@ -30,6 +30,34 @@ class ViewController: UIViewController {
     }
     
     @IBAction func postRequest(_ sender: Any) {
+        guard let url = URL(string: "https://jsonplaceholder.typicode.com/posts")
+        else { return }
+        let userData = ["Course": "Networking", "Lesson": "GET and POST Requests"]
+        
+        var request = URLRequest(url: url)
+        
+        request.httpMethod = "POST"
+        
+        guard let httpBody = try? JSONSerialization.data(withJSONObject: userData, options: []) else { return }
+        
+        request.httpBody = httpBody
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        
+        let session = URLSession.shared
+        session.dataTask(with: request) { data, response, error in
+            guard let response = response,
+                  let data = data else { return }
+            print(response)
+            
+            do {
+                let json = try JSONSerialization.jsonObject(with: data, options: [])
+                print(json)
+            } catch {
+                print(error)
+            }
+
+        }.resume()
+        
         
     }
 }
